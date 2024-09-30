05112b43a4cec7b71a108fcb276940ea6e1bf44b
Matt Keeley <keeley55@me.com>
2024-09-30 14:38:15 -0700

    root cmd handler

---
+++ cmd/root.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+    Use:   "awsss",
+    Short: "AWS Security Scanner",
+    Long:  "A simple AWS Security Scanner tool to analyze AWS IAM roles.",
+}
+
+func Execute() {
+    if err := rootCmd.Execute(); err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+}
